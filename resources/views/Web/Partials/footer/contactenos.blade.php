78955fd36665650b14a7b391be565a289b8d89bc
MeryJ39 <sandovalmery484@gmail.com>
2024-11-08 18:11:05 -0400

    Reiniciar proyecto de Laravel desde cero

--- resources/views/Web/Partials/footer/contactenos.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('welcome')
-
-@section('contenido')
-<div class="container mx-auto px-4 py-6">
-    <h1 class="text-2xl font-semibold">Contáctenos</h1>
-    <p>Aquí va la información de contacto y un formulario si lo deseas.</p>
-</div>
-@endsection
--- resources/views/Web/Partials/footer/politica-privacidad.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('welcome')
-
-@section('contenido')
-<div class="container mx-auto px-4 py-6">
-    <h1 class="text-2xl font-semibold">Política de privacidad</h1>
-    <p>Aquí va nuestra política de privacidad y el uso de datos.</p>
-</div>
-@endsection
